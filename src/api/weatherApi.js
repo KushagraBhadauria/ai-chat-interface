c61befb1f1cfb9a3d99d15059c2fcc6856b582aa
Kushagra Bhadauria <94129983+KushagraBhadauria@users.noreply.github.com>
2025-05-23 17:37:35 +0530

    Update weatherApi.js

--- src/api/weatherApi.js
+++ src/api/weatherApi.js
@@ -15,6 +15,16 @@ export const fetchWeather = async (city) => {
       'tokyo': { temp: 22, description: 'Sunny', humidity: 60, city: 'Tokyo' },
       'london': { temp: 12, description: 'Rainy', humidity: 85, city: 'London' },
       'new york': { temp: 18, description: 'Partly Cloudy', humidity: 70, city: 'New York'},
+      'mumbai': { temp: 33, description: 'Mostly Cloudy', humidity: 82, city: 'mumbai'},
+      'new delhi': { temp: 40, description: 'Partly Cloudy', humidity: 70, city: 'new delhi'},
+      'jaipur': { temp: 40, description: 'Sunny', humidity: 38, city: 'jaipur'},
+      'kanpur': { temp: 37, description: 'Sunny', humidity: 26, city: 'kanpur'},
+      'benglore': { temp: 32, description: 'Sunny', humidity: 42, city: 'benglore'},
+      'jhasi': { temp: 37, description: 'Sunny', humidity: 26, city: 'jhasi'},
+      'noida': { temp: 37, description: 'Sunny', humidity: 26, city: 'noida'},
+      'gurugram': { temp: 37, description: 'Sunny', humidity: 26, city: 'gurugram'},
+      'faridabad': { temp: 37, description: 'Sunny', humidity: 26, city: 'kanpur'},
+
   };
 
   const data = mockData[city.toLowerCase()];
